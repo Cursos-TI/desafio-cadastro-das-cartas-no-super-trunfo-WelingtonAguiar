73e7b41bca3cd9f184b44a2719eba1fe925be5c7
Welington Aguiar <85399106+WelingtonAguiar@users.noreply.github.com>
2025-03-12 00:28:02 +0000

    Atualização

--- CartasSuperTrunfoNovato.c
+++ CartasSuperTrunfoNovato.c
@@ -1,24 +1,3 @@
-/*
-Nível Básico
-Desafio: nível novato
-Criando as Cartas do Super Trunfo
-
-Neste primeiro nível, seu objetivo é construir a base do nosso jogo Super Trunfo de Países: um sistema para cadastrar as cartas com informações sobre as cidades. 
-Você vai praticar a leitura de dados do usuário, o armazenamento em variáveis e a exibição dessas informações na tela.
-
-O que você vai fazer
-
-Criar um programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo. Para cada carta, o usuário deverá fornecer as seguintes informações:
-
-Estado: Uma letra de 'A' a 'H' (representando um dos oito estados). Tipo: char
-Código da Carta: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
-Nome da Cidade: O nome da cidade. Tipo: char[] (string)
-População: O número de habitantes da cidade. Tipo: int
-Área (em km²): A área da cidade em quilômetros quadrados. Tipo: float
-PIB: O Produto Interno Bruto da cidade. Tipo: float
-Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade. Tipo: int.
-*/
-
 #include <stdio.h>
 // Necessário para utilizar as funções fgets e strcspn.
 #include <string.h>
